3c1b605567624cb5ed682eb3cdebc1df3ea5495c
lukast <tinnes-lukas@gmx.de>
2020-06-01 19:30:37 +0200

    dco on Interpreter.py and Painter.py

---
+++ src/Painter.py
@@ -0,0 +1,6 @@
+class Painter(object):
+    """
+    Paints a picture/thing based on the given hierarchy, which was build in Interpreter.py
+    Surfaces are used to represent interactions between adjacent surfaces
+    """
+    pass

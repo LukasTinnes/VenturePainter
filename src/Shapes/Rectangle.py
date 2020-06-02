c0beb32e6fe3901f71aad645e9cc8546a4087b40
lukast <tinnes-lukas@gmx.de>
2020-06-02 19:31:40 +0200

    added empty abstract funtion with todos for completionist sake in Rectangle.py

--- src/Shapes/Rectangle.py
+++ src/Shapes/Rectangle.py
@@ -4,9 +4,13 @@ from Engine.Shapes.FinitelyBounded.ShapeCollections.Std.AAB import AAB
 
 class Rectangle(AAB, FESS):
 
+    def get_fuzziness(self):
+        #Todo implement
+        pass
+
     def __init__(self, position, width, height, identifier=-1, surface_info=None):
         AAB.__init__(self, position, width, height, identifier)
         self.surface_info = surface_info
 
-    def get_surface_info(self):
+    def get_surface_info(self) -> dict:
         return self.surface_info

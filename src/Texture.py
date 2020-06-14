165e669a3f99b24837e20170b7f861318ef6074f
Lukas Tinnes <lukas.tinnes@studen.uni-augsburg.de>
2020-06-14 15:38:10 +0200

    fixed off by one error in tex.growing, temporary (permanent lets be honest) workaround

--- src/Texture.py
+++ src/Texture.py
@@ -15,17 +15,17 @@ class Texture:
 
         for x in range(dimensions[0]):
             for y in range(dimensions[1]):
-                x_rand = random.randint(0, width-1)
-                y_rand = random.randint(0, height-1)
-                pixels_new_img[x,y] = pixels_img[x_rand, y_rand]
+                x_rand = random.randint(0, width - 1)
+                y_rand = random.randint(0, height - 1)
+                pixels_new_img[x, y] = pixels_img[x_rand, y_rand]
 
         return img
 
     def tiles(self, dimensions):
         img = Image.new("RGB", dimensions)
         print(int(dimensions[0] / self.image.size[0]), int(dimensions[1] / self.image.size[1]))
-        for x in range(int(dimensions[0] / self.image.size[0])):
-            for y in range(int(dimensions[1] / self.image.size[1])):
+        for x in range(int(dimensions[0] / self.image.size[0]) + 1):
+            for y in range(int(dimensions[1] / self.image.size[1]) + 1):
                 img.paste(self.image, (x * self.image.size[0], y * self.image.size[1]))
         return img
 
@@ -55,5 +55,5 @@ class Texture:
         img.paste(img_crop, (seed[0], seed[1]))
         pixels = img.load()
         while not (seed[0] == 0 or seed[1] == 0 or seed[2] == dimensions[1] or seed[3] == dimensions[2]):
-            pass # TODO
+            pass  # TODO
         return img

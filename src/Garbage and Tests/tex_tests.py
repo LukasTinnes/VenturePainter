944795b19ae5c1b19efae740ef1f3dc8a7140a0d
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2020-06-15 00:43:23 +0200

    Image growing better now. still not good

--- src/Garbage and Tests/tex_tests.py
+++ src/Garbage and Tests/tex_tests.py
@@ -4,5 +4,9 @@ import numpy
 
 img = Image.open(r"..\..\Resources\marble.png")
 tex = Texture(img)
-new_img = tex.growing((100,100), 1/16 * numpy.array([[1,2,1],[2,4,2],[1,2,1]]), [1,1,2,2], 1)
+#new_img = tex.stochastic((100,100))
+#kernel = numpy.array([[1,2,1],[2,4,2],[1,2,1]])
+kernel = numpy.array([[0.5,1,2,1,0.5],[0.5,2,4,2,0.5],[0.5,1,2,1,0.5]])
+kernel = kernel / numpy.sum(kernel)
+new_img = tex.growing((30,30), kernel, [1,1,20,20], 0.5)
 new_img.show()
--- src/Texture.py
+++ src/Texture.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import random
 import math
-
+import numpy
 
 class Texture:
 
@@ -51,14 +51,16 @@ class Texture:
         :param seed:
         :return:
         """
-        img = Image.new("RGB", dimensions)
-        img_crop = self.image.crop(seed)
+        img = Image.new("RGB", dimensions, (0xFF, 0x00, 0xFF))
+        img_crop = self.image.crop((seed[0], seed[1], seed[2]+1, seed[3]+1))
         img.paste(img_crop, (seed[0], seed[1]))
+        img.show()
         pixels = img.load()
         pixels_orig = self.image.load()
         while not (seed[0] == 0 and seed[1] == 0 and seed[2] == dimensions[0]-1 and seed[3] == dimensions[1]-1):
             empties = list(self.range_around_box(seed, img))
-            for empty in empties:
+            print(f"Empties: {empties}")
+            for empty in self.range_around_box(seed, img):
                 empties.remove(empty)
                 self.__growing(empty, pixels, pixels_orig, kernel, empties, seed, threshold)
 
@@ -76,62 +78,64 @@ class Texture:
 
     def range_around_box(self, seed, img):
         if not (seed[1] == 0 or seed[0] == 0):
-            print("lu")
             yield seed[0] - 1, seed[1] - 1
 
         if not seed[0] == 0:
-            print("left")
             for y in range(seed[1], seed[3] + 1):
                 yield seed[0] - 1, y
 
         if not (seed[3] == img.size[1] - 1 or seed[1] == 0):
-            print("ld")
             yield seed[0] - 1, seed[3] + 1
 
         if not seed[3] == img.size[1] - 1:
-            print("down")
             for x in range(seed[0], seed[2] + 1):
                 yield x, seed[3] + 1
 
         if not seed[3] == img.size[1] - 1:
-            print("rd")
             yield seed[2] + 1, seed[3] + 1
 
         if not seed[2] == img.size[1] - 1:
-            print("right")
             for y in range(seed[1], seed[3] + 1):
                 yield seed[2] + 1, y
 
         if not (seed[2] == img.size[1] - 1 or seed[1] == 0):
-            print("ru")
             yield seed[2] + 1, seed[1] - 1
 
         if not seed[1] == 0:
-            print("up")
             for x in range(seed[0], seed[2] + 1):
                 yield x, seed[1] - 1
 
 
     def __growing(self, point, pixels_new, pixels_orig, kernel, empties, seed, threshold):
-        print(point)
         candidates = []
+        #print("growing")
+        overall_color = (0, 0, 0)
         for x in range(self.image.size[0]):
             for y in range(self.image.size[1]):
                 acc = 0
-                for mask_x in range(-1, 2):
-                    for mask_y in range(-1, 2):
+                for mask_x in range(-int(kernel.shape[0]/2), int(kernel.shape[0]/2)+1):
+                    for mask_y in range(-int(kernel.shape[1]/2), int(kernel.shape[1]/2)+1):
                         if not (point[0] + mask_x < seed[0] or point[1] + mask_y < seed[1]):
-                            if not (point[0] + mask_x >= seed[2] or point[1] + mask_y >= seed[3]):
+                            if not (point[0] + mask_x > seed[2] or point[1] + mask_y > seed[3]):
                                 if not (x + mask_x < 0 or y + mask_y < 0):
                                     if not (x + mask_x >= self.image.size[0] or y + mask_y >= self.image.size[1]):
-                                        if not (x + mask_x, y + mask_y) in empties:
-                                            col = pixels_new[point[0] + mask_x, point[1] + mask_y]
-                                            col2 = pixels_orig[x + mask_x, y + mask_y]
-                                            farbe = ((col[0] + col2[0]) * kernel[mask_x+1, mask_y+1],
-                                                     (col[1] + col2[1]) * kernel[mask_x + 1, mask_y + 1],
-                                                     (col[2] + col2[2]) * kernel[mask_x + 1, mask_y + 1])
-                                            acc += (farbe[0] + farbe[1] + farbe[2]) / 3
+                                        if not (point[0] + mask_x, point[1] + mask_y) in empties:
+                                            x_new, y_new = point[0] + mask_x, point[1] + mask_y
+                                            col = pixels_new[x_new, y_new]
+                                            #if col[0] == 0xFF and col[1] == 0x00 and col[2] == 0xFF:
+                                            #    print("Something fucky")
+                                            col2 = pixels_orig[x + mask_x, y + mask_y] #TODO there is still a mistake here where the empties are never in consideration
+                                            overall_color = (overall_color[0] + abs(col[0] - col2[0]) * kernel[mask_x + int(kernel.shape[0]/2), mask_y + int(kernel.shape[0]/2)],
+                                                             overall_color[1] + abs(col[1] - col2[1]) * kernel[mask_x + int(kernel.shape[0]/2), mask_y + int(kernel.shape[0]/2)],
+                                                             overall_color[2] + abs(col[2] - col2[2]) * kernel[mask_x + int(kernel.shape[0]/2), mask_y + int(kernel.shape[0]/2)])
+
+                        acc += (overall_color[0] + overall_color[1] + overall_color[2]) / 3
+                        overall_color = (0, 0, 0)
+
                 if acc < threshold:
+                    print(acc)
+                    #print(f"Acc: {acc}")
                     candidates.append((x, y))
+
         winner = random.choice(candidates)
         pixels_new[point[0], point[1]] = pixels_orig[winner[0], winner[1]]

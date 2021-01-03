139d6151e487209c56dc89b253236548e30e07ff
Tobias Jungbluth <tobias.jungbluth.tj@gmail.com>
2021-01-03 22:13:56 +0100

    Remade Loader

--- src/Loader.py
+++ src/Loader.py
@@ -1,11 +1,5 @@
-import re
 import xml.etree.ElementTree as ET
-
-import numpy as np
-from Engine.Camera import Camera
-
-from src.Shapes.Rectangle import Rectangle
-from src.SurfaceInfo import SurfaceInfo
+import planar
 
 
 class Loader(object):
@@ -20,49 +14,42 @@ class Loader(object):
         """
         self.path = path
         self.id_count = 0
-        # Action dict, to map different shapes to their respective loading functions
-        self.action_dict = {"rect": self._load_rect}
 
     def load(self, filename):
         """
         Loads every child node of the root of a specific svg images' xml tree representation
         :param filename: filename of the svg image (in the resource folder)
         """
+        #TODO fileending descrimination
+        return self._load_svg(filename)
+
+    def _load_svg(self, filename):
         tree = ET.parse(self.path + filename)
         root = tree.getroot()
-        attrib = root.attrib
-        background = Rectangle(np.array([float(attrib["width"])/2, float(attrib["height"])/2]),
-                               float(attrib["width"]), float(attrib["height"]), -1)
 
-        cam = Camera(np.array([float(attrib["width"])/2, float(attrib["height"])/2]),
-                     int(attrib["width"]), int(attrib["height"]), anchor=(True, False))
+        # Load information on background size
+        root_attrib = root.attrib
+        root_width = float(root_attrib["width"])
+        root_height = float(root_attrib["height"])
 
+        """Load shapes"""
         shapes = []
+
+        # Create bounding boxes from children
         for child in root:
-            shapes.append(self._load_object(child, cam))
-        return background, shapes, cam
+            # TODO other shapes
+            # Extract parameters
+            child_attrib = child.attrib
+            child_x = float(child_attrib["x"])
+            child_y = float(child_attrib["y"])
+            child_width = float(child_attrib["width"])
+            child_height = float(child_attrib["height"])
 
-    def _load_object(self, child, cam):
-        """
-        Loads a specific tree element depending on its tag
-        :param child: tree element
-        :return: Fuzzy shape object representing the given tree element
-        """
-        # Filtering tag from svg namespace, because xml sux
-        tag = re.sub("{.*}", "", child.tag)
-        attrib = child.attrib
-        return self.action_dict[tag](attrib, cam)
+            # Build BBox
+            maximum = planar.Vec2(child_x + child_width, child_y + child_height)
+            minimum = planar.Vec2(child_x, child_y)
+            box = planar.BoundingBox([minimum, maximum])
 
-    def _load_rect(self, attrib, cam: Camera):
-        """
-        Loads a rectangle (gets called in load_object)
-        :param attrib: rectangle attributes
-        :return: The Fuzzy shape representing this rectangle
-        """
-        width = float(attrib["width"])
-        height = float(attrib["height"])
-        cam_position = np.array([float(attrib["x"]) + width/2, float(attrib["y"]) + height/2])
-        position = cam.cam_to_world(cam_position)
-        rect = Rectangle(position, width, height, self.id_count, SurfaceInfo(color=attrib["fill"]))
-        self.id_count += 1
-        return rect
+            # Append to shappe list
+            shapes.append(box)
+        return shapes

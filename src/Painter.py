70d3502f446fe3f6455641849fa85c6793ffd616
lukast <tinnes-lukas@gmx.de>
2021-01-03 23:43:11 +0100

    file ending discrimination

--- src/Painter.py
+++ src/Painter.py
@@ -1,7 +1,15 @@
-from PIL import Image, ImageDraw
-from Engine.Camera import Camera
-from src.Shapes.Rectangle import Rectangle
 import random
+from typing import List
+
+from PIL import Image, ImageDraw
+from pygame import Rect
+
+from src.Node import Node
+
+
+def get_draw_coords(shape):
+    # Todo png image coordinates from shape coordinates
+    pass
 
 
 class Painter:
@@ -13,23 +21,40 @@ class Painter:
     def __init__(self):
         pass
 
-    def paint(self, hierarchy, background: Rectangle, shapes: list, camera: Camera):
+    def image_scale(self, window: Rect, shapes: List[Rect], scaling_factor):
         """
-        Paints an image based on a relationship graph provided
+        Scales the viewing window and all other shapes, to obtain image bounds sth sth todo
+        :param window:
+        :param shapes:
+        :param scaling_factor:
         :return:
         """
-        img = Image.new("RGB", (int(background.width), int(background.height)))
+        window = window.fit(scaling_factor * window.width, scaling_factor * window.height)
+        for shape in shapes:
+            shape = shape.fit(scaling_factor * shape.width, scaling_factor * shape.height)
+        return Image.new("RGB", (int(window.width), int(window.height)))
+
+    def paint(self, hierarchy: List[Node], shapes: List[Rect], viewing_window: Rect):
+        """
+        Paints an image based on a relationship graph provided
+        :param hierarchy: interpreted shape hierarchy
+        :param shapes: list of shapes read by the Loader
+        :param viewing_window: viewing window of the hierarchy shapes
+        :return: png
+        """
+        img = self.image_scale(viewing_window, shapes, 2)
+
         draw = ImageDraw.ImageDraw(img)
         hs = list(zip(hierarchy, shapes))
         for i in range(len(hs)):
-            shape = hs[i][1]
             node = hs[i][0]
-            anchor = camera.world_to_cam([shape.left, shape.up])
-            draw.rectangle([(anchor[0], anchor[1]), (anchor[0] + shape.width, anchor[1] + shape.height)], fill=(random.randint(0,125),random.randint(0,125),random.randint(0,125)))
-        for i in range(len(hs)):
             shape = hs[i][1]
-            node = hs[i][0]
-            anchor = camera.world_to_cam([shape.left, shape.up])
-            draw.text((anchor[0], anchor[1]), f"{hash(shape)}", fill=(255, 255, 255))
+            anchor = get_draw_coords(shape)
+            draw.rectangle([(anchor[0], anchor[1]), (anchor[0] + shape.width, anchor[1] + shape.height)],
+                           fill=(random.randint(0, 125), random.randint(0, 125), random.randint(0, 125)))
+        # for i in range(len(hs)):
+        #     shape = hs[i][1]
+        #     node = hs[i][0]
+        #     anchor = camera.world_to_cam([shape.left, shape.up])
+        #     draw.text((anchor[0], anchor[1]), f"{hash(shape)}", fill=(255, 255, 255))
         return img
-

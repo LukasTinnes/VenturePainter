078c95ad7fb910a2013f7f120c3a36009b338844
lukast <tinnes-lukas@gmx.de>
2021-01-03 22:25:26 +0100

    load dialog

--- src/Main.py
+++ src/Main.py
@@ -1,13 +1,20 @@
-from src.Intrepreter import Interpreter
+import os
+from tkinter import *
+from tkinter import filedialog
+
 from src.Loader import Loader
-from src.Painter import Painter
 
 if __name__ == "__main__":
+    root = Tk()
+    root.withdraw()
+    root.filename = filedialog.askopenfilename(initialdir="./", title="Select file",
+                                               filetypes=[("SVG-Image", "*.svg"), ("All files", "*.*")])
+    folder_path = os.path.dirname(root.filename)
     loader = Loader("../Resources/")
     background, r_w, r_h = loader.load("complex.svg")
     print(background, r_w, r_h)
-    #interpreter = Interpreter()
-    #hierarchy = interpreter.interpret(background, shapes)
-    #painter = Painter()
-    #img = painter.paint(hierarchy, background, shapes, camera)
-    #img.show()
+    # interpreter = Interpreter()
+    # hierarchy = interpreter.interpret(background, shapes)
+    # painter = Painter()
+    # img = painter.paint(hierarchy, background, shapes, camera)
+    # img.show()

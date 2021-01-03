7706616ba8e01756e5a7ed9d225871ecad4c715a
lukast <tinnes-lukas@gmx.de>
2021-01-03 22:37:20 +0100

    file ending discrimination

--- src/Main.py
+++ src/Main.py
@@ -9,7 +9,9 @@ if __name__ == "__main__":
     root = Tk()
     root.withdraw()
     root.filename = filedialog.askopenfilename(initialdir="../", title="Select file",
-                                               filetypes=[("SVG-Image", "*.svg"), ("All files", "*.*")])
+                                               filetypes=[("SVG-Image", "*.svg"), ("png image", "*.png"),
+                                                          ("jpg image", "*.jpg *.jpeg"),
+                                                          ("tagged image file", "*.tiff *.tif"), ("All files", "*.*")])
     folder_path = os.path.dirname(root.filename)
     logging.basicConfig(filename='example.log', level=logging.INFO)
     loader = Loader(folder_path)

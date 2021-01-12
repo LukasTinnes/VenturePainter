2385ae2d903202be2fc463e390a6b1fd06ccfb15
lukast <tinnes-lukas@gmx.de>
2021-01-12 20:46:28 +0100

    Merge branch 'master' of /home/lukast/PycharmProjects/VenturePainter with conflicts.

--- src/Interface.py
+++ src/Interface.py
@@ -26,47 +26,31 @@ class Interface:
     """
 
     def __init__(self):
-        # Data
-        self.img = None
-        self.theme = None
-        self.shapes = None
-        self.hierarchy = None
-        self.action_dict = None
-
         # GUI Elements
         self.radio_labels = ["Uniform", "Neighbor"]
+
+        self.img = None
+
         self.fig = plt.figure()
-        self.gridspec = gridspec.GridSpec(5,2, height_ratios=[0.1, 0.1, 0.1, 0.1,0.9], width_ratios=[0.9,0.1])
+        self.gridspec = gridspec.GridSpec(3,2, height_ratios=[0.1, 0.1, 0.9], width_ratios=[0.9,0.1])
 
         self.img_ax = self.fig.add_subplot(self.gridspec[:,0])
-        self.paint_button_ax = self.fig.add_subplot(self.gridspec[0,1])
-        self.img_load_button_ax = self.fig.add_subplot(self.gridspec[1, 1])
-        self.theme_load_button_ax = self.fig.add_subplot(self.gridspec[2, 1])
-        self.save_button_ax = self.fig.add_subplot(self.gridspec[3,1])
-        self.radio_button_ax = self.fig.add_subplot(self.gridspec[4,1])
+        self.calculate_button_ax = self.fig.add_subplot(self.gridspec[0,1])
+        self.save_button_ax = self.fig.add_subplot(self.gridspec[1,1])
+        self.radio_button_ax = self.fig.add_subplot(self.gridspec[2,1])
 
         self.radio_buttons = RadioButtons(self.radio_button_ax, self.radio_labels)
-        self.imgLoadButton = Button(self.img_load_button_ax, "Load Image")
-        self.themeLoadButton = Button(self.theme_load_button_ax, "Load Theme")
-        self.paintButton = Button(self.paint_button_ax, "Paint")
+        self.calculateButton = Button(self.calculate_button_ax, "Calculate")
         self.saveButton = Button(self.save_button_ax, "Save")
 
-        def img_load(*args):
+        def calculate(*args):
             filename = self.get_filename([("SVG-Image", "*.svg"), ("png image", "*.png"),
                                            ("jpg image", "*.jpg *.jpeg"),
                                            ("tagged image file", "*.tiff *.tif"),
                                            ("All files", "*.*")], "Select image")
-            self.shapes, self.hierarchy = self.load_img(filename)
-        self.imgLoadButton.on_clicked(img_load)
-
-        def theme_load(*args):
-            self.theme, self.action_dict = self.load_theme()
-        self.themeLoadButton.on_clicked(theme_load)
-
-        def paint(*args):
-            self.img = self.paint()
+            self.img = self.generate_img(filename)
             self.img_ax.imshow(self.img)
-        self.paintButton.on_clicked(paint)
+        self.calculateButton.on_clicked(calculate)
 
         def save(*args):
             if self.img is not None:
@@ -89,34 +73,21 @@ class Interface:
         logging.info(f"Acquired file name")
         return root.filename
 
-    def load_img(self, filename):
-        """
-        Loads the image
-        :param filename:
-        :return:
-        """
+    def generate_img(self, filename):
+        theme, js = self.get_theme()
+        action_dict = {key: SurfaceInfoNoContext.from_json(js[key]) for key in js.keys()}
+        logging.info("Loaded action dict")
+
         loader = Loader()
         shapes = loader.load(filename)
         interpreter = Interpreter()
         hierarchy = interpreter.interpret(shapes)
 
-        return shapes, hierarchy
 
-    def load_theme(self):
-        """
-        Loads the theme and action dict
-        :return:
-        """
-        theme, js = self.get_theme()
-        action_dict = {key: SurfaceInfoNoContext.from_json(js[key]) for key in js.keys()}
-        return theme, action_dict
-
-    def paint(self):
-        interpreter = Interpreter()
-        interpreter.determine_kind(self.hierarchy, self.shapes, self.theme)
+        interpreter.determine_kind(hierarchy, shapes, theme)
         painter = Painter()
-        viewing_window = pygame.Rect(0, 0, 500, 500)
-        return painter.paint(self.hierarchy, self.shapes, viewing_window, self.action_dict)
+        viewing_window = pygame.Rect(0, 0, 1500, 1500)
+        return painter.paint(hierarchy, shapes, viewing_window, action_dict)
 
     def get_theme(self):
         file_name = self.get_filename([("json theme", "*.json"), ("All files", "*.*")], "Select theme")
